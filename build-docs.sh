77d0073b7716c3099fc5a078175c8265948493fb
David Hewitt <mail@davidhewitt.dev>
2025-11-06 13:26:04 +0000

    fixup pydantic_core symlink

--- build-docs.sh
+++ build-docs.sh
@@ -14,15 +14,14 @@ python3 -V
 python3 -m pip install --user uv
 python3 -m uv sync --all-packages --python 3.12 --group docs --frozen
 python3 -m uv tool run maturin develop -m pydantic-core/Cargo.toml
-python3 -m uv pip list
 python3 -m uv run --no-sync python -c 'import docs.plugins.main'
 
 # Adding local symlinks gets nice source locations like
 #   pydantic_core/core_schema.py
 # instead of
-#   .venv/lib/python3.10/site-packages/pydantic_core/core_schema.py
+#   pydantic-core/python/pydantic_core/core_schema.py
 # See also: mkdocs.yml:mkdocstrings:handlers:python:paths: [.]:
-ln -s .venv/lib/python*/site-packages/pydantic_core pydantic_core
+ln -s pydantic-core/python/pydantic_core pydantic_core
 ln -s .venv/lib/python*/site-packages/pydantic_settings pydantic_settings
 ln -s .venv/lib/python*/site-packages/pydantic_extra_types pydantic_extra_types
 # Put these at the front of PYTHONPATH (otherwise, symlinked
