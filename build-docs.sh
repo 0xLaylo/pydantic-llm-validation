4edd5c4cf2ea55186fecaf67848a0e35f287130a
David Hewitt <mail@davidhewitt.dev>
2025-11-06 13:34:57 +0000

    try uv direct install

--- build-docs.sh
+++ build-docs.sh
@@ -6,15 +6,16 @@
 set -e
 set -x
 
+# Install Rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source $HOME/.cargo/env
 
-python3 -V
+# Install uv
+curl -LsSf https://astral.sh/uv/install.sh | sh
 
-python3 -m pip install --user uv
-python3 -m uv sync --all-packages --python 3.12 --group docs --frozen
-python3 -m uv tool run maturin develop -m pydantic-core/Cargo.toml
-python3 -m uv run --no-sync python -c 'import docs.plugins.main'
+uv sync --all-packages --python 3.12 --group docs --frozen
+uv tool run maturin develop -m pydantic-core/Cargo.toml
+uv run --no-sync python -c 'import docs.plugins.main'
 
 # Adding local symlinks gets nice source locations like
 #   pydantic_core/core_schema.py
@@ -26,4 +27,4 @@ ln -s .venv/lib/python*/site-packages/pydantic_settings pydantic_settings
 ln -s .venv/lib/python*/site-packages/pydantic_extra_types pydantic_extra_types
 # Put these at the front of PYTHONPATH (otherwise, symlinked
 # entries will still have "Source code in .venv/lib/.../*.py ":
-PYTHONPATH="$PWD${PYTHONPATH:+:${PYTHONPATH}}" python3 -m uv run --no-sync mkdocs build
+PYTHONPATH="$PWD${PYTHONPATH:+:${PYTHONPATH}}" uv run --no-sync mkdocs build
